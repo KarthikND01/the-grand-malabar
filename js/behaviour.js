8009d5642560ee879d6319cbf23d9f6e41c8f2a6
KarthikND01 <132391706+KarthikND01@users.noreply.github.com>
2023-05-14 12:07:55 -0400

    Added Responsive Heading

--- js/behaviour.js
+++ js/behaviour.js
@@ -10,3 +10,29 @@ function closeImagePopup() {
   var popup = document.getElementById("imagePopup");
   popup.style.display = "none";
 }
+
+window.addEventListener("resize", function () {
+  var heading = document.getElementById("responsiveHeading");
+  var windowWidth = window.innerWidth;
+
+  if (windowWidth < 480) {
+    heading.innerHTML =
+      "<i class='fa fa-utensils me-3'></i>The Grand Malabar<br>Indian Cuisine";
+  } else {
+    heading.innerHTML =
+      "<i class='fa fa-utensils me-3'></i>The Grand Malabar Indian Cuisine";
+  }
+});
+
+function initiateUI() {
+  var heading = document.getElementById("responsiveHeading");
+  var windowWidth = window.innerWidth;
+
+  if (windowWidth < 480) {
+    heading.innerHTML =
+      "<i class='fa fa-utensils me-3'></i>The Grand Malabar<br>Indian Cuisine";
+  } else {
+    heading.innerHTML =
+      "<i class='fa fa-utensils me-3'></i>The Grand Malabar Indian Cuisine";
+  }
+}
